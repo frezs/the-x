fa3a3989eb8707ca247acd6c7f36615804a90ab3
guzi <270835426@qq.com>
2024-05-07 20:58:30 +0800

    add: 首页navbar

---
+++ data/mock/common.ts
@@ -0,0 +1,29 @@
+export const header = {
+  logo: '/next.svg',
+  navLinks: [
+    {
+      name: '首页',
+      href: '/'
+    },
+    {
+      name: '资讯',
+      href: '/news'
+    },
+    {
+      name: 'AI',
+      href: '/ai'
+    },
+    {
+      name: '电商',
+      href: '/mall'
+    },
+    {
+      name: '导航',
+      href: '/nav'
+    },
+    {
+      name: '关于',
+      href: '/about'
+    }
+  ]
+}

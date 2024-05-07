99a9a2ba4ceaf0ff94b7b757f6143e5cdbe0e037
gyz <270835426@qq.com>
2024-05-07 22:50:00 +0800

    add: 配置unocss icon

--- uno.config.ts
+++ uno.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
     presetUno(),
     presetAttributify(),
     presetIcons({
+      collections: {
+        mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default),
+        'simple-line-icons': () => import('@iconify-json/simple-line-icons/icons.json').then(i => i.default)
+      },
       // Optional
       extraProperties: {
         display: 'inline-block', 'vertical-align': 'middle'

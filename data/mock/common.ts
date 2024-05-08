e63256869c904b69a93cc6e1f5157553bcde3ea6
guzi <270835426@qq.com>
2024-05-08 18:29:49 +0800

    add: 更新mock数据

--- data/mock/common.ts
+++ data/mock/common.ts
@@ -1,29 +1,54 @@
+import { IPostRecommend } from '@/components/post-recommend'
+
+export const recommend: IPostRecommend = {
+  module: ' 推荐 ',
+  icon: 'i-mdi:book',
+  color: '',
+  ext: [{ name: '投稿', url: '#' }, {
+    name: '更多',
+    url: '#'
+  }],
+  items: Array(5).fill({
+    url: '#',
+    cover: { url: '/next.svg', name: '封面' },
+    tags: [{ url: '#', name: '标签' }],
+    author: { url: '#', name: '作者' },
+    title: '现在看到的这里是标题，现在看到的这里是标题，',
+    desc: '这是一段描述信息，这是一段描述信息，这是一段描述信息，这是一段描述信息，',
+    create: '3小时前',
+    times: '阅读11分钟'
+  })
+}
+
+export const navLinks = [
+  {
+    name: '首页',
+    path: '/'
+  },
+  {
+    name: '资讯',
+    path: '/news'
+  },
+  {
+    name: 'AI',
+    path: '/ai'
+  },
+  {
+    name: '电商',
+    path: '/mall'
+  },
+  {
+    name: '导航',
+    path: '/nav'
+  },
+  {
+    name: '关于',
+    path: '/about'
+  }
+]
+
 export const header = {
   logo: '/next.svg',
-  navLinks: [
-    {
-      name: '首页',
-      href: '/'
-    },
-    {
-      name: '资讯',
-      href: '/news'
-    },
-    {
-      name: 'AI',
-      href: '/ai'
-    },
-    {
-      name: '电商',
-      href: '/mall'
-    },
-    {
-      name: '导航',
-      href: '/nav'
-    },
-    {
-      name: '关于',
-      href: '/about'
-    }
-  ]
+  navLinks,
+  recommend
 }
